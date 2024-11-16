3c6be7f8fb0efe55acfbae8d28040d5516097ee0
Yan Lima Barbosa <yanbr763@gmail.com>
2024-11-16 17:27:49 -0300

    feat: fastify plugins

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,10 @@
 import fastify from 'fastify'
-import { knex } from './database'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  const transaction = await knex('transactions')
-    .where('amount', 1000)
-    .select('*')
-
-  return transaction
-})
+app.register(transactionsRoutes)
 
 app
   .listen({
